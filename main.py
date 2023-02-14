bb1b3fcc7f81fb8bfea5c66961e37b950244f552
Harján Krisztián <erik00000@hotmail.com>
2023-02-14 10:38:23 +0100

    pizzeria-web

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+@app.route("/rendeles")
+def etlap():
+    return render_template("rendeles.html")
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+@app.route("/meretek")
+def meretek():
+    return render_template("meretek.html")
+
+@app.route("/pizzak")
+def pizzak():
+    return render_template("pizzak.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
